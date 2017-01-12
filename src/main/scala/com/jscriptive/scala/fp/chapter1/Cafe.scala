a73fc75c1f8abb57e36ec396ab5e23b2f0f4ad53
Gerd Reiss <gerdreiss@jscriptive.com>
2017-01-12 21:30:12 +0100

    FP in Scala Chapter 1

---
+++ src/main/scala/com/jscriptive/scala/fp/chapter1/Cafe.scala
@@ -0,0 +1,50 @@
+package com.jscriptive.scala.fp.chapter1
+
+import scala.collection.immutable.Seq
+
+class Coffee(val flavor: String = "espresso", val price: Int = 500 /* cents */)
+
+class CreditCard(val number: String) {
+  def charge(amount: Int): Unit =
+    println(s"Charging $amount")
+
+  override def toString: String = number
+}
+
+class Payments {
+  def charge(cc: CreditCard, price: Int): Unit =
+    println(s"Charging creditcard $cc $price cents")
+}
+
+case class Charge(cc: CreditCard, amount: Int) {
+  def combine(other: Charge): Charge =
+    if (cc == other.cc)
+      Charge(cc, amount + other.amount)
+    else
+      throw new Exception("Can't combine charge to different cards")
+}
+
+class CafeWithSideEffects {
+  def buyCoffee(cc: CreditCard, p: Payments): Coffee = {
+    val cup = new Coffee()
+    p.charge(cc, cup.price) // <- side effect
+    cup
+  }
+}
+
+class CafeWithoutSideEffects {
+  def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+    val cup = new Coffee()
+    (cup, Charge(cc, cup.price))
+  }
+
+  def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+    val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+    val (coffees: Seq[Coffee], charges: Seq[Charge]) = purchases.unzip
+    (coffees, charges.reduce(_ combine _))
+  }
+
+  def coalesce(charges: List[Charge]): List[Charge] =
+    charges.groupBy(_.cc).values.map(_.reduce(_ combine _)).toList
+}
+
