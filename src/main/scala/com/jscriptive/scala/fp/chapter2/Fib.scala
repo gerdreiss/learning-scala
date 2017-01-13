82d5732d5c01285428071d90337db9c78189cbaa
Gerd Reiss <gerdreiss@jscriptive.com>
2017-01-13 21:53:07 +0100

    FP in Scala Chapter 2

---
+++ src/main/scala/com/jscriptive/scala/fp/chapter2/Fib.scala
@@ -0,0 +1,18 @@
+package com.jscriptive.scala.fp.chapter2
+
+import scala.annotation.tailrec
+
+object Fib {
+
+  def fib(n: Int): Int = {
+    @tailrec def rec(counter: Int = 1, prev: Int = 0, curr: Int = 1): Int =
+      if (counter == n) prev
+      else rec(counter + 1, curr, prev + curr)
+
+    rec()
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(fib(6))
+  }
+}
