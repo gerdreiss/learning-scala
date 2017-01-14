5afc9b006c72cbe25cfb1ffc7ffc9b001cd236ed
Gerd Reiss <gerdreiss@jscriptive.com>
2017-01-14 18:17:59 +0100

    FP in Scala Chapter 2

--- src/main/scala/com/jscriptive/scala/fp/chapter2/Sorted.scala
+++ src/main/scala/com/jscriptive/scala/fp/chapter2/Sorted.scala
@@ -7,12 +7,10 @@ object Sorted {
   }
 
   def isSorted2[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-
     def loop(n: Int): Boolean =
       if (n == as.length - 1) true
       else if (!ordered(as(n), as(n + 1))) false
       else loop(n + 1)
-
     loop(0)
   }
 
