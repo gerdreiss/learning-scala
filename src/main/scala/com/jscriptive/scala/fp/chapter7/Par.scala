8211716ac48a8bb51ad5ff6bb7465d6dd5063a68
Gerd Reiss <gerdreiss@jscriptive.com>
2017-02-01 20:59:09 +0100

    chapter 7

--- src/main/scala/com/jscriptive/scala/fp/chapter7/Par.scala
+++ src/main/scala/com/jscriptive/scala/fp/chapter7/Par.scala
@@ -8,14 +8,20 @@ object Par {
 
   type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
-
   private case class UnitFuture[A](get: A) extends Future[A] {
     override def isDone = true
     override def get(timeout: Long, units: TimeUnit): A = get
     override def isCancelled = false
     override def cancel(evenIfRunning: Boolean) = false
   }
+
+  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
+
+  def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    a => lazyUnit(f(a))
 
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
     (es: ExecutorService) => {
@@ -24,10 +30,24 @@ object Par {
       UnitFuture(f(af.get, bf.get))
     }
 
-  def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+  def map[A, B](p: Par[A])(f: A => B): Par[B] =
+    map2(p, unit(()))((a, _) => f(a))
 
+  def sequence[A](as: List[Par[A]]): Par[List[A]] = as match {
+    case Nil => unit(Nil)
+    case h :: t => map2(h, fork(sequence(t)))(_ :: _)
+  }
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] =
+    sequence(ps.map(asyncF(f)))
+
+  def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] = {
+    val pars: List[Par[List[A]]] =
+      l.map(asyncF((a: A) => if (f(a)) List(a) else List()))
+    map(sequence(pars))(_.flatten)
+  }
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 }
 
 
