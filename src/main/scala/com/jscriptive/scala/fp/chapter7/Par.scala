6f53c369df160a59ba0e7ab7912953c0e0dfb253
Gerd Reiss <gerdreiss@jscriptive.com>
2017-02-04 19:08:43 +0100

    chapter 7

--- src/main/scala/com/jscriptive/scala/fp/chapter7/Par.scala
+++ src/main/scala/com/jscriptive/scala/fp/chapter7/Par.scala
@@ -16,30 +16,24 @@ object Par {
   }
 
   def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
-
-  def fork[A](a: => Par[A]): Par[A] = es => es.submit(() => a(es).get)
-  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
-
-  def asyncF[A, B](f: A => B): A => Par[B] =
-    a => lazyUnit(f(a))
+  def delay[A](a: => Par[A]): Par[A] = es => a(es)
+  def lazyUnit[A](a: => A): Par[A] = delay(unit(a))
+  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
 
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
-    (es: ExecutorService) => {
-      val af = a(es)
-      val bf = b(es)
-      UnitFuture(f(af.get, bf.get))
+    (es: ExecutorService) => UnitFuture(f(a(es).get, b(es).get))
+
+  def map[A, B](a: Par[A])(f: A => B): Par[B] =
+    map2(a, unit(()))((a, _) => f(a))
+
+  def sequence[A](as: List[Par[A]]): Par[List[A]] =
+    as match {
+      case Nil => unit(Nil)
+      case h :: t => map2(h, delay(sequence(t)))(_ :: _)
     }
 
-  def map[A, B](p: Par[A])(f: A => B): Par[B] =
-    map2(p, unit(()))((a, _) => f(a))
-
-  def sequence[A](as: List[Par[A]]): Par[List[A]] = as match {
-    case Nil => unit(Nil)
-    case h :: t => map2(h, fork(sequence(t)))(_ :: _)
-  }
-
-  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] =
-    sequence(ps.map(asyncF(f)))
+  def parMap[A, B](as: List[A])(f: A => B): Par[List[B]] =
+    sequence(as.map(asyncF(f)))
 
   def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] = {
     val pars: List[Par[List[A]]] =
@@ -48,6 +42,17 @@ object Par {
   }
 
   def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def equal[A](es: ExecutorService)(a1: Par[A], a2: Par[A]): Boolean =
+    a1(es).get == a2(es).get
+
+  def main(args: Array[String]): Unit = {
+    // Define Laws for our API
+    // map(unit(x))(f) == unit(f(x))
+    // map(unit(x))(id) == unit(id(x))
+    // map(unit(x))(id) == unit(x)
+    // map(y)(id) == y
+  }
 }
 
 
