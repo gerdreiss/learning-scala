b870bad324117bdbbb8d0a9f48a7d30a1eaf5f7e
Gerd Reiss <gerdreiss@jscriptive.com>
2017-02-22 21:36:08 +0100

    chapter 8

--- src/main/scala/com/jscriptive/scala/fp/chapter8/Prop.scala
+++ src/main/scala/com/jscriptive/scala/fp/chapter8/Prop.scala
@@ -1,8 +1,15 @@
 package com.jscriptive.scala.fp.chapter8
 
+import com.jscriptive.scala.fp.chapter8.Prop.{FailedCase, SuccessCount}
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+}
+
 trait Prop {
-  def check: Boolean
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
   def &&(p: Prop): Prop = new Prop {
-    override def check: Boolean = super.check && p.check
+    override def check: Either[(FailedCase, SuccessCount), SuccessCount] = ???
   }
 }
