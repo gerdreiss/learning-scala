7d1a24f0910e036a98e6612a4624a18783a54a52
Reiss Gerd <Gerd.Reiss@finnova.com>
2017-07-21 11:34:00 +0200

    Lens pattern fixed

--- src/main/scala/com/jscriptive/scala/advanced/Lens.scala
+++ src/main/scala/com/jscriptive/scala/advanced/Lens.scala
@@ -1,23 +1,29 @@
 package com.jscriptive.scala.advanced
 
+import scala.util.Try
+
 final case class Lens[O, V](get: O => Option[V], set: (O, V) => Try[O]) {
 
   /** */
   def >>[W](inner: Lens[V, W]): Lens[O, W] =
     Lens[O, W](
-      this.get(_).flatMap(inner.get(_)),
-      (o, w) => inner.set(get(o), w).flatMap(i => set(o, i))
+      this.get(_).flatMap(inner.get),
+      (o, w) => get(o).map(_o => inner.set(_o, w).flatMap(i => set(o, i))).get
     )
 
 }
 
-case class Inner2(val s: String)
-case class Inner1(val s: String, val inner: Inner2)
-case class Outer(val s: String, val inner: Inner1)
+case class Inner2(name: String)
+case class Inner1(name: String, inner: Inner2)
+case class Outer(name: String, inner: Inner1)
 
-object Lens {
+object Lens extends App {
 
   val inner1: Lens[Outer, Inner1] = Lens(o => Option(o.inner), (o, i) => Try(o.copy(inner = i)))
-  val inner2: Lens[Outer, Inner2] = inner1 >> Lens(inner1 = Option(i1.inner), (inner1, inner2) => Try(inner1.copy(inner2 = inner2)))
+  val inner2: Lens[Outer, Inner2] = inner1 >> Lens(o => Option(o.inner), (inner1, inner2) => Try(inner1.copy(inner = inner2)))
+
+  val outer = Outer("outer", Inner1("inner1", Inner2("inner2")))
+
+  println(inner2.get(outer))
 
 }
