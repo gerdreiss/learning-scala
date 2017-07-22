3d95558850d319aaf656db593fae8290dace1e27
Gerd Reiss <gerdreiss@jscriptive.com>
2017-07-22 09:23:31 +0200

    scala meta programming

--- src/main/scala/com/jscriptive/scala/advanced/Reflections.scala
+++ src/main/scala/com/jscriptive/scala/advanced/Reflections.scala
@@ -1,25 +1,25 @@
 package com.jscriptive.scala.advanced
 
-import scala.reflect.runtime.{universe => ru}
+import scala.reflect.runtime.universe._
 
 object Reflections extends App {
 
   case class Person(name: String)
   case class Purchase(name: String, orderNumber: Int, var shipped: Boolean)
 
-  def getTypeTag[T: ru.TypeTag](obj: T): ru.TypeTag[T] = ru.typeTag[T]
+  def getTypeTag[T: TypeTag](obj: T): TypeTag[T] = typeTag[T]
 
   val person = Person("Mike")
 
-  val theType: ru.Type = getTypeTag(person).tpe
-  val classPerson: ru.ClassSymbol = ru.typeOf[Person].typeSymbol.asClass
+  val theType: Type = getTypeTag(person).tpe
+  val classPerson: ClassSymbol = typeOf[Person].typeSymbol.asClass
   println(theType)
   println(classPerson)
 
-  val m: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader)
-  val cm: ru.ClassMirror = m.reflectClass(classPerson)
-  val ctor: ru.MethodSymbol = ru.typeOf[Person].decl(ru.termNames.CONSTRUCTOR).asMethod
-  val ctorm: ru.MethodMirror = cm.reflectConstructor(ctor)
+  val m: Mirror = runtimeMirror(getClass.getClassLoader)
+  val cm: ClassMirror = m.reflectClass(classPerson)
+  val ctor: MethodSymbol = typeOf[Person].decl(termNames.CONSTRUCTOR).asMethod
+  val ctorm: MethodMirror = cm.reflectConstructor(ctor)
   println(cm)
   println(ctor)
   println(ctorm)
@@ -28,8 +28,8 @@ object Reflections extends App {
   println(mp)
 
   val purchase = Purchase("Jeff Lebowski", 23819, shipped = false)
-  val shippingTermSymb: ru.TermSymbol = ru.typeOf[Purchase].decl(ru.TermName("shipped")).asTerm
-  val im: ru.InstanceMirror = m.reflect(purchase)
+  val shippingTermSymb: TermSymbol = typeOf[Purchase].decl(termNames("shipped")).asTerm
+  val im: InstanceMirror = m.reflect(purchase)
   val shippingFieldMirror = im.reflectField(shippingTermSymb)
   println(shippingFieldMirror.get)
 
