343c4c0f8072c48e05a1221a03a8a5a354ec2d70
Reiss Gerd <Gerd.Reiss@finnova.com>
2017-10-18 17:09:09 +0200

    lfpis control structures

---
+++ src/main/scala/fp/lfpis/ControlStructureUsingThreeParamGroups.scala
@@ -0,0 +1,15 @@
+package fp.lfpis
+
+/**
+ * Created by grei on 18.10.2017
+ */
+object ControlStructureUsingThreeParamGroups extends App {
+
+  def ifBothTrue(p1: => Boolean)(p2: => Boolean)(f: => Unit): Unit = {
+    if (p1 && p2) f
+  }
+
+  ifBothTrue(p1 = true)(p2 = true) {
+    println("both true")
+  }
+}
