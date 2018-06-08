da884869e54fa1d257e347fa36b54ea6922aa522
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-08 08:41:57 +0200

    Exercism.io exercise 2 fixed

--- src/main/scala/exercism/SumOfMultiples.scala
+++ src/main/scala/exercism/SumOfMultiples.scala
@@ -4,7 +4,7 @@ import scala.language.postfixOps
 
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int =
-    (1 to limit) filter { n =>
+    (1 until limit) filter { n =>
       factors exists(n % _ == 0)
     } sum
 }
