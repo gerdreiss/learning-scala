195a1ffd734c67cb2a464d9c77149836d92bbc34
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-13 15:36:38 +0200

    grad school optimized

--- src/main/scala/exercism/School.scala
+++ src/main/scala/exercism/School.scala
@@ -6,17 +6,17 @@ import scala.language.postfixOps
 class School {
   type DB = Map[Int, Seq[String]]
 
-  private val _db = mutable.Map.empty[Int, Seq[String]]
+  private val _db = mutable.TreeMap.empty[Int, Seq[String]]
 
   def add(name: String, g: Int): Unit =
-    _db.update(g, grade(g) :+ name)
+    _db update(g, grade(g) :+ name)
 
-  def db: DB = _db toMap
+  def db: DB =
+    _db toMap
 
   def grade(g: Int): Seq[String] =
     _db getOrElse(g, Seq.empty)
 
-  def sorted: DB = _db.toSeq.sortBy(_._1) map {
-    case (g, names) => (g, names.sorted)
-  } toMap
+  def sorted: DB =
+    _db mapValues (_.sorted) toMap
 }
