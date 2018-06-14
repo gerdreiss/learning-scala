a8e40f9c17f4c8898b2b5f4ec430c6588809d8ae
Reiss Gerd <Gerd.Reiss@finnova.com>
2018-06-14 14:22:35 +0200

    sum of multiples, variant with for comprehension

--- src/main/scala/exercism/SumOfMultiples.scala
+++ src/main/scala/exercism/SumOfMultiples.scala
@@ -5,7 +5,15 @@ import scala.language.postfixOps
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int =
     (1 until limit) filter { n =>
-      factors exists(n % _ == 0)
+      factors exists (n % _ == 0)
     } sum
+
+  def sum2(factors: Set[Int], limit: Int): Int =
+    (for {
+      f <- factors
+      n <- 1 until limit
+      if f * n < limit
+    } yield f * n) sum
+
 }
 
