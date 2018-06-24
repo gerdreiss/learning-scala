7733670b54709626e01f8b2bf925fbba98d5b165
Gerd Reiss <gerdreiss@jscriptive.com>
2018-06-24 14:21:08 +0200

    Trying another thing from Haskell

--- src/main/scala/fp/HaskellInScala.scala
+++ src/main/scala/fp/HaskellInScala.scala
@@ -10,9 +10,28 @@ object HaskellInScala extends App {
     func1,
     func2,
     func3
-  ) map (_(4.0))
+  ) map (_ (4.0))
 
   println(list.mkString("\n"))
   println(list.sum)
 
+
+  //  coins :: (Ord a, Num a) => [a]
+  //  coins = [2, 3, 5, 10]
+  def coins = Seq(2, 3, 5, 10)
+
+  //  change :: (Ord a, Num a) => a -> [[a]]
+  //  change 0   = [[]]
+  //  change sum = [c : r | c <- coins, c <= sum, r <- change (sum - c)]
+  def change(sum: Int): Seq[Seq[Int]] = sum match {
+    case 0 => Seq(Seq.empty)
+    case _ => for {
+      c <- coins
+      if c <= sum
+      r <- change(sum - c)
+    } yield c +: r
+  }
+
+  println(change(7))
+  println(change(17))
 }
