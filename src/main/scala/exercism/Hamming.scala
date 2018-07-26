f527ffc345cafbc1e7df49519890ddcc9259c804
Gerd Reiss <gerdreiss@jscriptive.com>
2018-07-26 20:48:32 +0200

    Exercism, Scala Track, Hamming

--- src/main/scala/exercism/Hamming.scala
+++ src/main/scala/exercism/Hamming.scala
@@ -1,3 +1,5 @@
+package exercism
+
 object Hamming {
   def distance(str1: String, str2: String): Option[Int] =
     if (str1.length != str2.length) None
@@ -8,4 +10,11 @@ object Hamming {
         }
       }
 
+  def distance1(str1: String, str2: String): Option[Int] =
+    (str1, str2) match {
+      case (a, b) if a.length == b.length => Some {
+        (a zip b) count (t => t != t.swap)
+      }
+      case _ => None
+    }
 }
