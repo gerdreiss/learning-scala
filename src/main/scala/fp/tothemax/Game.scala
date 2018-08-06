5e44c6d243bbe32143630cbf0ce2aacca605ac65
gerdreiss <gerdreiss@jscriptive.com>
2018-08-06 19:16:13 +0200

    Programming a guessing game with John de Goes: start refactoring to use ConsoleOut

--- src/main/scala/fp/tothemax/Game.scala
+++ src/main/scala/fp/tothemax/Game.scala
@@ -12,6 +12,36 @@ import TestAlgebra._
   */
 object Game extends App {
 
+  sealed trait ConsoleOut {
+    def en: String
+  }
+  object ConsoleOut {
+    case class YouGuessedRight(name: String) extends ConsoleOut {
+      def en = s"You guessed right, $name!"
+    }
+    case class YouGuessedWrong(name: String, num: Int) extends ConsoleOut {
+      def en = s"You guessed wrong, $name! The number was: $num"
+    }
+    case class DoYouWantToContinue(name: String) extends ConsoleOut {
+      def en = s"Do you want to continue, $name?"
+    }
+    case class PleaseGuess(name: String) extends ConsoleOut {
+      def en = s"Dear $name, please guess a number from 1 to 5: "
+    }
+    case class ThatIsNotValid(name: String) extends ConsoleOut {
+      def en = s"That is not a valid selection, $name!"
+    }
+    case object WhatIsYourName extends ConsoleOut {
+      def en = "What is your name?"
+    }
+    case class WelcomeToGame(name: String) extends ConsoleOut {
+      def en = s"Hello, $name, welcome to the game!"
+    }
+    case object NotANumber extends ConsoleOut {
+      def en = "You did not enter a number"
+    }
+  }
+
   def parseInt(s: String): Option[Int] = Try(s.toInt).toOption
 
   def finish[F[_]: Program, A](a: => A): F[A] = Program[F].finish(a)
