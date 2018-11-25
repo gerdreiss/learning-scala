7172f016b4c9577f831a1c69891074877d1adf8f
Gerd Reiss <gerdreiss@jscriptive.com>
2018-11-25 13:09:23 +0100

    to be or not to be

---
+++ src/main/scala/typesystem/Shakespeare.scala
@@ -0,0 +1,18 @@
+package typesystem
+
+import scala.language.postfixOps
+
+object Shakespeare extends App {
+
+  val or = "or"
+  val to = "or"
+
+  object To {
+    def be(o: or.type) = this
+    def not(t: to.type) = this
+    def be = "That is the question"
+  }
+
+  println (To be or not to be)
+
+}
