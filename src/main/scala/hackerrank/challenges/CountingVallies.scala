cb1a8b8fba9752f555adb5e02de03740c468afde
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-08-15 14:44:34 +0200

    https://www.hackerrank.com/challenges/counting-valleys/problem

--- src/main/scala/hackerrank/challenges/CountingVallies.scala
+++ src/main/scala/hackerrank/challenges/CountingVallies.scala
@@ -1,22 +1,28 @@
 package hackerrank.challenges
 
 object CountingVallies extends App {
-  // Complete the countingValleys function below.
+
   def countingValleys(n: Int, s: String): Int =
     s.map {
       case 'D' => -1
-      case 'U' => 1
-      case _ => 0
+      case 'U' =>  1
+      case _   =>  0
     }
-      .foldLeft((0, 0)) {
-        case ((valleys, acc), current) =>
-          val newAcc = acc + current
-          val newVallyes = if (current == 1 && newAcc == 0) valleys + 1 else valleys
-          (newVallyes, newAcc)
-      }
-      ._1
+    .scan(0)(_ + _)
+    .sliding(2)
+    .count(_ == Seq(-1, 0))
+    //s.map {
+    //  case 'D' => -1
+    //  case 'U' => 1
+    //  case _   => 0
+    //}
+    //.foldLeft((0, 0)) {
+    //  case ((valleys, acc), current) =>
+    //    val newAcc = acc + current
+    //    val newVallyes = if (current == 1 && newAcc == 0) valleys + 1 else valleys
+    //    (newVallyes, newAcc)
+    //}
+    //._1
 
   println(countingValleys(0, "UDDDUDUU"))
-  println(countingValleys(0, "UDDDUDUUDUUUDDDDDUU"))
-  println(countingValleys(0, "DDUU"))
 }
