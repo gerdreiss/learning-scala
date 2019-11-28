9e9527ca73ec7ac31c880396e3da2eedb8485feb
Reiss Gerd <Gerd.Reiss@finnova.com>
2019-11-28 14:15:55 +0100

    Split Algebras and Domain

--- pfps/shoppingcart/src/main/scala/shoppingcart/Domain.scala
+++ pfps/shoppingcart/src/main/scala/shoppingcart/Domain.scala
@@ -1,5 +1,8 @@
 package shoppingcart
 
+import java.time.LocalDate
+import java.util.UUID
+
 import io.estatico.newtype.macros._
 import eu.timepit.refined._
 import eu.timepit.refined.api._
@@ -9,16 +12,65 @@ import eu.timepit.refined.types.string.NonEmptyString
 
 object Domain {
 
-  type TUsername = String Refined MatchesRegex[W.`"""^[a-zA-Z0-9_]{6,}$"""`.T]
-  type TEmail    = String Refined MatchesRegex[W.`"""^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"""`.T]
+  @newtype case class UserId(value: UUID)
+  @newtype case class UserName(value: String)
+  @newtype case class Password(value: String)
+  @newtype case class JwtToken(value: String)
 
-  @newtype case class Username(value: TUsername)
-  @newtype case class Email(value: TEmail)
+  @newtype case class BrandId(value: UUID)
+  @newtype case class BrandName(value: String)
+  @newtype case class Brand(value: String)
 
-  // usage:
-  //import io.estatico.newtype.ops._
-  //"name@mail.com".coerce[Email]
+  @newtype case class CategoryId(value: UUID)
+  @newtype case class Category(value: String)
 
-  //def lookup(username: NonEmptyString): F[Option[User]]
+  @newtype case class ItemId(value: UUID)
+  @newtype case class ItemName(value: String)
+  @newtype case class ItemDescription(value: String)
+
+  @newtype case class Quantity(value: Int)
+  @newtype case class Cart(items: Map[ItemId, Quantity])
+  @newtype case class CartId(value: UUID)
+
+  @newtype case class OrderId(uuid: UUID)
+  @newtype case class PaymentId(uuid: UUID)
+
+  @newtype case class USD(value: BigDecimal)
+
+  case class Item(
+    uuid: ItemId,
+    name: ItemName,
+    description: ItemDescription,
+    price: USD,
+    brand: Brand,
+    category: Category
+  )
+
+  case class CreateItem(
+    name: ItemName,
+    description: ItemDescription,
+    price: USD,
+    brandId: BrandId,
+    categoryId: CategoryId
+  )
+
+  case class UpdateItem(
+    id: ItemId,
+    price: USD
+  )
+
+  case class CartItem(item: Item, quantity: Quantity)
+  case class CartTotal(items: List[CartItem], total: USD)
+
+  case class Order(
+    id: OrderId,
+    pid: PaymentId,
+    items: Map[ItemId, Quantity],
+    total: USD
+  )
+
+  case class User(id: UserId, name: UserName)
+  case class Card(number: String, validUntil: LocalDate)
+
 
 }
