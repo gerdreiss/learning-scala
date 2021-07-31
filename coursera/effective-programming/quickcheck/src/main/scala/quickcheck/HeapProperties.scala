0890247582d177cca47f24b2df3edc617f1bf12f
Gerd Reiss <gerd@reiss.pro>
2021-07-31 19:01:51 +0200

    Effective Programming in Scala Week 4: Quickcheck - implementation

--- coursera/effective-programming/quickcheck/src/main/scala/quickcheck/HeapProperties.scala
+++ coursera/effective-programming/quickcheck/src/main/scala/quickcheck/HeapProperties.scala
@@ -22,19 +22,18 @@ class HeapProperties(heapInterface: HeapInterface) extends Properties("Heap"):
   property("the minimum of a heap of two elements should be the smallest of the two elements") =
     forAll { (x1: Int, x2: Int) =>
       val heap = insert(x2, insert(x1, empty))
-      val min: Int = ???
+      val min: Int = math.min(x1, x2)
       findMin(heap) == min
     }
 
   property("delete minumum of heap of one element should return an empty heap") =
     forAll { (x: Int) =>
       // create a heap with exactly one element, `x`
-      val heap1: List[Node] = ???
+      val heap1: List[Node] = insert(x, empty)
       // delete the minimal element from it
-      val heap0: List[Node] = ???
+      val heap0: List[Node] = deleteMin(heap1)
       // check that heap0 is empty
-      ???
-      
+      heap0.isEmpty
     }
 
   property("continually finding and deleting the minimal element of a heap should return a sorted sequence") =
@@ -46,15 +45,15 @@ class HeapProperties(heapInterface: HeapInterface) extends Properties("Heap"):
         true
       else
         // find the minimal element
-        val x1: Int = ???
+        val x1: Int = findMin(heap)
         // delete the minimal element of `heap`
-        val heap2: List[Node] = ???
+        val heap2: List[Node] = deleteMin(heap)
         // find the minimal element in `heap2`
-        val x2: Int = ???
+        val x2: Int = findMin(heap2)
         // check that the deleted element is smaller than the minimal element
         // of the remaining heap, and that the remaining heap verifies the
         // same property (by recursively calling `check`)
-        val checked: Boolean = ???
+        val checked: Boolean = x1 <= x2
         checked
     // check arbitrary heaps
     forAll { (heap: List[Node]) =>
@@ -62,8 +61,33 @@ class HeapProperties(heapInterface: HeapInterface) extends Properties("Heap"):
     }
 
   // TODO Write more properties here to detect the bugs
-  // in bogus BinomialHeap implementations
-  
+  //      in bogus BinomialHeap implementations
+  property("given a heap and a sequence of integers, insert all to heap and repeatly finding and deleting return the intial sequence of integers") =
+    def check(seq: Seq[Int], heap: List[Node]): Boolean =
+      if heap.isEmpty || seq.isEmpty then
+        true
+      else
+        val min = findMin(heap)
+        seq.head == min && check(seq.tail, deleteMin(heap))
+
+    def ins(xs: Seq[Int], acc: List[Node]): List[Node] =
+      if xs.isEmpty then acc
+      else ins(xs.tail, insert(xs.head, acc))
+
+    forAll { (seq: Seq[Int]) =>
+      var heap = ins(seq, empty)
+      check(seq.sorted, heap)
+    }
+
+  property("finding a minimum of melding of any two heaps should return a minimum of one or the other") =
+    forAll { (heap1: List[Node], heap2: List[Node]) =>
+      if heap1.isEmpty || heap2.isEmpty then true
+      else
+        val min1 = findMin(heap1)
+        val min2 = findMin(heap2)
+        val minM = meld(heap1, heap2)
+        findMin(minM) == math.min(min1, min2)
+    }
 
   // random heap generator --- DO NOT MODIFY
   private lazy val genHeap: Gen[List[Node]] = oneOf(const(empty),
